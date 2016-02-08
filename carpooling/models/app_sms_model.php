168ad270e97b0a3e1280786fc7ff76639a8ce76a
Shashikant Farakate <shashikant.farakate123@gmail.com>
2016-02-08 22:48:30 +0530

    changes in booking

--- carpooling/models/app_sms_model.php
+++ carpooling/models/app_sms_model.php
@@ -4,14 +4,14 @@ class App_sms_model extends CI_Model
 {
 	function send_otp($mobno,$otp)  
     {   
-        $api_url = "http://www.logonutility.in/enterprise/app/smsapi/index.php?key=356601BDA902DC&campaign=5887&routeid=20&type=text&contacts=".$mobno."&senderid=OPAUTO&msg=Message+from+GoGreenRyde+:+your+phone+verification+code+is+".$otp."";
-		$response = file_get_contents($api_url);
+        $api_url="http://www.logonutility.in/app/smsapi/index.php?key=356601BDA902DC&campaign=5887&routeid=20&type=text&contacts=".$mobno."&senderid=DEMOLG&msg=Message+from+GoGreenRyde+:+your+phone+verification+code+is+".$otp."";
+		return $response = file_get_contents($api_url);
     }
 
     function alert_guardian($mobno,$msg)  
     {   
-        $api_url = "http://www.logonutility.in/enterprise/app/smsapi/index.php?key=356601BDA902DC&campaign=5887&routeid=20&type=text&contacts=".$mobno."&senderid=OPAUTO&msg=".$msg."";
-		$response = file_get_contents($api_url);
+        $api_url="http://www.logonutility.in/app/smsapi/index.php?key=356601BDA902DC&campaign=5887&routeid=20&type=text&contacts=".$mobno."&senderid=DEMOLG&msg=".$msg."";
+		return $response = file_get_contents($api_url);
     }
 }    
 ?>
